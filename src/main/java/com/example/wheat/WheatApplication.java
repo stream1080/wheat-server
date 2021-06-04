cdbca4eb1cce3c583f38940ab2509032e9722d13
stream1080 <fmxhax638@foxmail.com>
2021-06-04 16:26:02 +0800

    集成Swagger

--- src/main/java/com/example/wheat/WheatApplication.java
+++ src/main/java/com/example/wheat/WheatApplication.java
@@ -1,9 +1,11 @@
 package com.example.wheat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.wheat")
 public class WheatApplication {
 
     public static void main(String[] args) {
