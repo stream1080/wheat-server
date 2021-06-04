84953b492a28d5dd24d2e4ac28dfb931508a02b0
stream1080 <fmxhax638@foxmail.com>
2021-06-04 16:01:51 +0800

    Initial commit

---
+++ src/main/java/com/example/wheat/WheatApplication.java
@@ -0,0 +1,13 @@
+package com.example.wheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WheatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wheat/WheatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wheat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
